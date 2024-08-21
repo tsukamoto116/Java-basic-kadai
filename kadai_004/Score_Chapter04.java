e9627d196c79575a270163467ff1f0adee8a6754
tsukamoto116 <kaztuka0116@gmail.com>
2024-08-22 08:31:37 +0900

    課題：テストの平均点

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,24 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		// 点数を表示
+		
+		System.out.println("Aさんの点数は10点");
+		System.out.println("Bさんの点数は20点");
+		System.out.println("Cさんの点数は30点");
+		System.out.println("Dさんの点数は40点");
+		System.out.println("Eさんの点数は50点");
+		System.out.println("Fさんの点数は60点");
+		System.out.println("Gさんの点数は70点");
+		System.out.println("Hさんの点数は80点");
+		System.out.println("Iさんの点数は90点");
+		System.out.println("Jさんの点数は100点");
+		
+		// 平均点を求める
+		System.out.println("テストの平均点は" + (100 + 90 + 80 + 70 + 60 + 50 + 40 +30 + 20 + 10) / 10 + "点");
+
+	}
+
+}
